e6b4be478884222d4e47762b28322a1987691cec
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-07-13 12:55:56 +0530

    Project Setup

---
+++ src/routes/Allroutes.js
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+
+import React from "react";
+import MovieDetail from "../pages/MovieDetail";
+import MovieList from "../pages/MovieList";
+import Search from "../pages/Search";
+import PageNotFound from "../pages/PageNotFound";
+
+const Allroutes = () => {
+  return (
+    <>
+      <Routes>
+        <Route />
+      </Routes>
+    </>
+  );
+};
+
+export default Allroutes;
